ef627c2eb969c978217b4172f17e69355b6ed30d
momolly1024 <momolly1024@gmail.com>
2022-06-02 16:20:51 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useContext, createContext, useState } from 'react'
+import {
+    Link,
+    Route,
+    Routes,
+    Navigate,
+    Outlet,
+    useLocation,
+    useNavigate,
+} from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Account = () => {
+    return <p> (Protected) Account page</p>
+}
+const Home = () => {
+    return <p> (Protected) Home Page</p>
+}
+const PageLinks = () => {
+    return (
+        <div>
+            <Link to='/'> Sign In Page</Link>
+            <Link to='/home'> Home Page</Link>
+            <Link to='/account'> Account Page</Link>
+        </div>
+    )
+}
+const SignIn = () => {
+    return <p>Sign In Page</p>
+}
+const Views = () => {
+    return (
+        <Routes>
+            <Route path='/' element={<SignIn />} />
+            <Route element={<ProtectedRoutes />}>
+                <Route path='/home' element={<Home />} />
+                <Route path='/account' element={<Account />} />
+            </Route>
+        </Routes>
+    )
+}
+const useAuth = () => {
+    const { user } = useContext(UserContext)
+    return user && user.loggedIn
 }
 
-export default App;
+const ProtectedRoutes = () => {
+    const location = useLocation()
+    const isAuth = useAuth()
+    return isAuth ? (
+        <Outlet />
+    ) : (
+        <Navigate to='/' replace state={{ from: location }} />
+    )
+}
+
+const LogInButtons = () => {
+    const { user, setUser } = useContext(UserContext)
+    const navigate = useNavigate()
+    const location = useLocation()
+    return (
+        <div>
+            <p>{`Logged In: ${user.loggedIn}`}</p>
+            <button
+                onClick={() => {
+                    if (user.loggedIn) return
+                    setUser({ loggedIn: true })
+
+                    if (location.state?.from) {
+                        navigate(location.state.from)
+                    }
+                }}
+            >
+                Log In
+            </button>
+            <button
+                onClick={() => {
+                    if (!user.loggedIn) return
+                    setUser({ loggedIn: false })
+                }}
+            >
+                Log Out
+            </button>
+        </div>
+    )
+}
+export const UserContext = createContext()
+
+function App() {
+    const [user, setUser] = useState({ loggedIn: false })
+    return (
+        <UserContext.Provider value={{ user, setUser }}>
+            <LogInButtons />
+            <PageLinks />
+            <Views />
+        </UserContext.Provider>
+    )
+}
+
+export default App
